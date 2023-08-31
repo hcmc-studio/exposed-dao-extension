f8c7461a8f867091519c61adfe09e97ae4a8a1a9
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-31 20:55:44 +0900

    [Script push]

---
+++ src/main/kotlin/studio/hcmc/exposed/dao/EntityID.kt
@@ -0,0 +1,7 @@
+package studio.hcmc.exposed.dao
+
+import org.jetbrains.exposed.dao.id.EntityID
+
+typealias IntEntityId = EntityID<Int>
+typealias LongEntityId = EntityID<Long>
+typealias StringEntityId = EntityID<String>
